51f316d71488ece22267ea4928e211ab73584f04
CristianNicolae <76454832+crissty21@users.noreply.github.com>
2022-12-03 20:25:50 +0200

    init

---
+++ Project1/MyForm.h
@@ -0,0 +1,380 @@
+﻿#pragma once
+#include <list>
+#include <msclr\marshal_cppstd.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace std;
+
+
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+		cli::array<System::Windows::Forms::Label^, 2>^ texts = gcnew cli::array<Label^, 2>(12, 12); //cream un vector de obiecte Lable pentru afisarea numarului sub bari
+	public:
+		cli::array<int, 2>^ b = gcnew cli::array<int, 2>(12, 12);
+		cli::array<int, 2>^ coordonate = gcnew cli::array<int, 2>(4, 1000);
+
+		cli::array<int, 4>^ drumuri = gcnew cli::array<int, 4>(12,12,122,3);
+
+		PictureBox^ fundal = gcnew PictureBox();
+
+		int limita;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+
+
+
+	public:
+	public:
+
+	public:
+
+
+	public:
+		System::ComponentModel::Container^ components;
+
+		MyForm(void)
+		{
+			InitializeComponent();
+		}
+
+		//fuctie responsabila de desenarea patratelelor 
+		void desenare()
+		{
+			char aux[3];
+			//culorile patratelelor
+			int C = 230, c;
+			//dimensiuni si locatii
+			int dim_x = 40, dim_y = 40, spatiu_sus=50, spatiu_lateral=100, spatiu_intre = 3;
+
+			Controls->Add(fundal);
+			fundal->Size = Drawing::Size(11 * (dim_x + spatiu_intre) + spatiu_intre, 11 * (dim_y + spatiu_intre) + spatiu_intre);
+			fundal->Location = Point(spatiu_lateral + dim_x, spatiu_sus+dim_y);
+			fundal->BackColor = Color::SkyBlue;
+
+			for(int i=1;i<=11;i++)
+				for (int j = 1; j <= 11; j++)
+				{
+					//cream obiectele
+					texts[i, j] = gcnew Label();
+
+					//adaugam patratele cu text
+					this->Controls->Add(texts[i, j]);
+
+					//setam dimensiunile si locatile in form
+					texts[i, j]->Location = Point(j * (dim_x + spatiu_intre) + spatiu_lateral, i * (dim_y + spatiu_intre) + spatiu_sus);
+					texts[i, j]->Size = Drawing::Size(dim_x, dim_y);
+
+					
+					//setam culoarea
+					//daca patratelul este teleport il coloram in verde, iar valoare afisata va fi 0
+					if (b[i, j] == -1)
+					{
+						texts[i, j]->BackColor = Color::Green;
+						texts[i, j]->Text = "0";
+					}
+					else
+					{
+						itoa(b[i,j],aux,10);
+						c = C - 40 * b[i, j];
+						texts[i, j]->BackColor = Color::FromArgb(static_cast<Int32>(static_cast<Byte>(c)), static_cast<Int32>(static_cast<Byte>(c)), static_cast<Int32>(static_cast<Byte>(c)));
+						texts[i, j]->Text = msclr::interop::marshal_as<String^>(aux);
+					}
+					texts[i, j]->BringToFront();
+					texts[i, j]->TextAlign = ContentAlignment::MiddleCenter;
+					texts[i, j]->ForeColor = Color::Black;
+					texts[i, j]->Font = (gcnew Drawing::Font(L"Times New Roman", 12.0F, Drawing::FontStyle::Bold, Drawing::GraphicsUnit::Point,static_cast<Byte>(0)));
+				}
+			
+		}
+		void miscari()
+		{
+			Color cul;
+			char aux[3];
+			for (int i = 1; i <= limita; i++)
+			{
+				if (i > 1)
+				{
+					texts[coordonate[1, i - 1], coordonate[2, i - 1]]->BackColor = cul;
+					texts[coordonate[1, i - 1], coordonate[2, i - 1]]->Refresh();
+
+				}
+				cout << coordonate[1, i] << ", " << coordonate[2, i] << endl;
+				cul = texts[coordonate[1, i], coordonate[2, i]]->BackColor;
+				if (coordonate[3, i] != INT_MAX)
+				{
+					itoa(coordonate[3, i], aux, 10);
+					texts[coordonate[1, i], coordonate[2, i]]->Text = msclr::interop::marshal_as<String^>(aux);
+					texts[coordonate[1, i], coordonate[2, i]]->ForeColor = Color::FromArgb(static_cast<Int32>(static_cast<Byte>(255)), static_cast<Int32>(static_cast<Byte>(60)), static_cast<Int32>(static_cast<Byte>(60)));
+					
+				}
+				texts[coordonate[1, i], coordonate[2, i]]->BackColor = Color::FromArgb(static_cast<Int32>(static_cast<Byte>(255)), static_cast<Int32>(static_cast<Byte>(128)), static_cast<Int32>(static_cast<Byte>(128)));
+				texts[coordonate[1, i], coordonate[2, i]]->Refresh();
+
+				Sleep(100);
+			}
+			texts[coordonate[1, limita], coordonate[2, limita]]->BackColor = cul;
+			texts[coordonate[1, limita], coordonate[2, limita]]->Refresh();
+			label3->Text = texts[11, 11]->Text;
+			label3->BackColor = pictureBox1->BackColor;
+			label3->Font = (gcnew Drawing::Font(L"Times New Roman", 20, Drawing::FontStyle::Bold, Drawing::GraphicsUnit::Point, static_cast<Byte>(0)));
+		}
+		void desenare_drum(int x, int y)
+		{
+			for (int i = 1; i <= drumuri[x, y, 0, 0]; i++)
+			{
+				cout << drumuri[x, y, i, 1] << " " << drumuri[x, y, i, 2] << endl;
+				texts[drumuri[x, y, i, 1], drumuri[x, y, i, 2]]->BackColor = Color::Aquamarine;
+			}
+			texts[x, y]->BackColor = Color::Aquamarine;
+		}
+		void reparare_drum(int x, int y)
+		{
+			if(x==-1)
+			{
+				x = 1; y = 1;
+			}
+			char aux[3];
+			int c, C = 230;
+			for (int i = 1; i <= drumuri[x, y, 0, 0]; i++)
+			{
+				if (b[drumuri[x, y, i, 1], drumuri[x, y, i, 2]] == -1)
+					texts[drumuri[x, y, i, 1], drumuri[x, y, i, 2]]->BackColor = Color::Green;
+				else
+				{
+
+					itoa(b[drumuri[x, y, i, 1], drumuri[x, y, i, 2]], aux, 10);
+					c = C - 40 * b[drumuri[x, y, i, 1], drumuri[x, y, i, 2]];
+					texts[drumuri[x, y, i, 1], drumuri[x, y, i, 2]]->BackColor = Color::FromArgb(static_cast<Int32>(static_cast<Byte>(c)), static_cast<Int32>(static_cast<Byte>(c)), static_cast<Int32>(static_cast<Byte>(c)));
+
+				}
+			}
+			if (b[x,y] == -1)
+				texts[x,y]->BackColor = Color::Green;
+			else
+			{
+
+				itoa(b[x,y], aux, 10);
+				c = C - 40 * b[x,y];
+				texts[x,y]->BackColor = Color::FromArgb(static_cast<Int32>(static_cast<Byte>(c)), static_cast<Int32>(static_cast<Byte>(c)), static_cast<Int32>(static_cast<Byte>(c)));
+
+			}
+		}
+	protected:
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+#pragma region Windows Form Designer generated code
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->button1->Location = System::Drawing::Point(821, 184);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(127, 49);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"calculeaza distanta pana la finish";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->button2->Location = System::Drawing::Point(821, 259);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(127, 49);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Evidentiaza drumul cel mai optim";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->button4->Location = System::Drawing::Point(922, 532);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"dreapta";
+			this->button4->UseVisualStyleBackColor = false;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// button5
+			// 
+			this->button5->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->button5->Location = System::Drawing::Point(772, 532);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"stanga";
+			this->button5->UseVisualStyleBackColor = false;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
+			// button6
+			// 
+			this->button6->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->button6->Location = System::Drawing::Point(848, 557);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(75, 23);
+			this->button6->TabIndex = 5;
+			this->button6->Text = L"jos";
+			this->button6->UseVisualStyleBackColor = false;
+			this->button6->Click += gcnew System::EventHandler(this, &MyForm::button6_Click);
+			// 
+			// button7
+			// 
+			this->button7->BackColor = System::Drawing::SystemColors::ButtonFace;
+			this->button7->Location = System::Drawing::Point(848, 506);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(75, 23);
+			this->button7->TabIndex = 6;
+			this->button7->Text = L"sus";
+			this->button7->UseVisualStyleBackColor = false;
+			this->button7->Click += gcnew System::EventHandler(this, &MyForm::button7_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->pictureBox1->Location = System::Drawing::Point(751, 169);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(261, 437);
+			this->pictureBox1->TabIndex = 7;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(769, 457);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(228, 13);
+			this->label1->TabIndex = 8;
+			this->label1->Text = L"Muta casuta pentru care se evidentiaza drumul";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(804, 338);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(161, 13);
+			this->label2->TabIndex = 9;
+			this->label2->Text = L"Cel mai optim drum are lungimea:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->label3->Location = System::Drawing::Point(860, 377);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(0, 13);
+			this->label3->TabIndex = 10;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1111, 655);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+		bool drumul_este_calculat = false;
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		reparare_drum(X,Y);
+		miscari();
+		drumul_este_calculat = true;
+	}
+		   int X=-1, Y;
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (drumul_este_calculat) {
+		X = 11;
+		Y = 11;
+		desenare_drum(X, Y);
+	}
+}
+
+private: System::Void button7_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (X > 1)
+	{
+		reparare_drum(X, Y);
+		X--;
+		desenare_drum(X, Y);
+	}
+}
+private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (X <11 && X!=-1)
+	{
+		reparare_drum(X, Y);
+		X++;
+		desenare_drum(X, Y);
+	}
+}
+private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (Y > 1 && X != -1)
+	{
+		reparare_drum(X, Y);
+		Y--;
+		desenare_drum(X, Y);
+	}
+}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (Y < 11 && X != -1)
+	{
+		reparare_drum(X, Y);
+		Y++;
+		desenare_drum(X, Y);
+	}
+
+}
+};
+}
---
+++ Project1/Source.cpp
@@ -0,0 +1,321 @@
+﻿#include "MyForm.h"
+#include <windows.h>
+#include <fstream>
+
+
+using namespace Project1;
+
+int mutari[4][1000], k = 1;
+void verificare();
+//transpunem labiritul intr-un graf
+//clasa nodurilor 
+//fiecare patratel va reprezenta un nod
+class node {
+public:
+	//penalizarea aplicata pe aceasta casuta
+	int penalitate = 0;
+	//distanta dintre tile si start
+	//daca distanta dintre start si patratelul curent este infinit, inseamna ca nu a fost calculata inca
+	int distanta_start = INT_MAX;
+	//numarul de casute adiacente
+	//teleportul are "un vecin" in plus
+	int numar_vecini;
+	//referinte la casutele vecine
+	node* vecini[6];
+
+
+	//vecinul care duce spre start
+	node* tata;
+
+	//daca casuta curenta este sau nu teleport
+	bool teleport = false;
+
+	//coordonatele tile-ului 
+	//indici pe care ii vom folosi la afisarea drumului final
+	int indice_x, indice_y;
+
+	//toti vecinii pot fi tati
+	//alegem tatal cu distanta pana la start cea mai mica
+	int alege_tata()
+	{
+		//in cea mai mare parte din cazuri, primul vecin are o distanta pana la start cunoscuta
+		//vom incepe cu el
+		int tatal_favorit = vecini[1]->distanta_start;
+		tata = vecini[1];
+
+		//daca ne aflam pe un teleport, vom alege dintre toti vecinii distanta cea mai mica
+		if (teleport)
+		{
+			//alegem tatal cu distanta pana la start minima
+			for (int i = 2; i <= numar_vecini; i++)
+				if (vecini[i]->distanta_start < tatal_favorit)
+				{
+					//salvam distanta pana la start a tatalui preferan
+					tatal_favorit = vecini[i]->distanta_start;
+					//salvam tatal pregerat
+					tata = vecini[i];
+				}
+		}
+		//daca nu ne aflam, trebuie sa avem grija la un posibil vecin teleport
+		else
+		{
+			//parcurgem toti vecinii
+			for (int i = 2; i <= numar_vecini; i++)
+				if (vecini[i]->distanta_start < tatal_favorit)
+				{
+					//daca avem un vecin teleport, il putem folosi pe post de tata, doar daca acesta la randul sau are tata pe celalalt teleport
+					if (vecini[i]->teleport)
+					{
+						if (vecini[i]->tata->teleport)
+						{
+							//salvam distanta pana la start a tatalui preferan
+							tatal_favorit = vecini[i]->distanta_start;
+							//salvam tatal preferat
+							tata = vecini[i];
+						}
+					}
+					else
+					{
+						//salvam distanta pana la start a tatalui preferan
+						tatal_favorit = vecini[i]->distanta_start;
+						//salvam tatal preferat
+						tata = vecini[i];
+					}
+				}
+		}
+		//Returnam distanta optima pana la start
+
+		//daca nu am gasit un tata cu o distanta pana la start finita, ne vom asigura ca returnam o distanta care sa nu distruga programul :)) - hope that makes sens
+		if (tatal_favorit == INT_MAX)
+		{
+			//in functia rezolvare, facem adunarea dintre valoare din tatal_favorit si penalitate. Daca tatal favorit retine INT_MAX, iar noi ii adunam ceva, il dam peste cap :)
+			return tatal_favorit - penalitate;
+		}
+		return tatal_favorit;
+	}
+	//functie responsabila de actualizarea copiilor 
+	void actualizare_copii()
+	{
+		//daca ne aflam pe teleport, verificam daca tatal sau este tot teleport sau nu 
+		//daca este, atunci actualizam vecinii, iar daca nu, actualizam cel de al doilea teleport
+		if (teleport)
+		{
+			if (tata->teleport)
+			{
+				//daca ne-am teleportat aici, avem voie sa actualizam vecinii, indiferent daca sunt finiti sau nu 
+				//nu luam in calcul si ultimul vecin, care este teleportul
+				for (int i = 1; i < numar_vecini; i++)
+				{
+					if (vecini[i]->distanta_start > vecini[i]->penalitate + distanta_start)
+					{
+						//actualizam atat distanta pana la start, cat si tatal
+						vecini[i]->distanta_start = vecini[i]->penalitate + distanta_start;
+						mutari[1][k] = vecini[i]->indice_x;
+						mutari[2][k] = vecini[i]->indice_y;
+						mutari[3][k] = vecini[i]->distanta_start;
+						k++;
+						vecini[i]->tata = this;
+						//actualizam copiii copilului
+						vecini[i]->actualizare_copii();
+					}
+				}
+			}
+			else
+			{
+				//daca tatal teleportului nu este cel de al doilea teleport, inseamna ca am ajuns aici de pe o casuta adiacenta
+				//fapt ce ne spune ca singura casuta unde putem merge este celalalt teleport
+				if (vecini[numar_vecini]->distanta_start >= distanta_start)
+				{
+					//daca il modificam, ii actualizam copiii
+					vecini[numar_vecini]->distanta_start = distanta_start;
+					vecini[numar_vecini]->tata = this;
+					mutari[1][k] = vecini[numar_vecini]->indice_x;
+					mutari[2][k] = vecini[numar_vecini]->indice_y;
+					mutari[3][k] = vecini[numar_vecini]->distanta_start;
+					k++;
+					vecini[numar_vecini]->actualizare_copii();
+				}
+			}
+		}
+		else
+		{
+			//daca nu este teleport, verificat distanta pana la start a tuturor vecinilor
+			for (int i = 1; i <= numar_vecini; i++)
+			{
+				//daca copiii au o distanta pana la start diferita de int_max, inseamna ca am mai trecut pe la ei
+				//fapt ce determina actualizarea distantei 
+				//aceasta conditie asigura ca nu facem un numar de miscari de ordinul miilor
+				if (vecini[i]->distanta_start != INT_MAX && vecini[i]->distanta_start > vecini[i]->penalitate + distanta_start)
+				{
+					vecini[i]->distanta_start = vecini[i]->penalitate + distanta_start;
+					vecini[i]->tata = this;
+					mutari[1][k] = vecini[numar_vecini]->indice_x;
+					mutari[2][k] = vecini[numar_vecini]->indice_y;
+					mutari[3][k] = vecini[i]->distanta_start;
+					k++;
+					vecini[i]->actualizare_copii();
+				}
+			}
+		}
+	}
+};
+ifstream lab("date.in");
+node* start, * finish;
+
+node* graf[12][12];
+int inaltime = 11, latime = 11;
+//functie responsabila de citirea datelor din fisier
+void citire()
+{
+	//retine unul dintre teleporturi
+	node* gateway = NULL;
+	//contor pentru memorarea numarului de vecini
+	int k;
+
+	//cream matricea nodurilor
+	for (int i = 1; i <= inaltime; i++)
+		for (int j = 1; j <= latime; j++)
+			graf[i][j] = new node();
+
+	//citirea grafului
+	for (int i = 1; i <= inaltime; i++)
+		for (int j = 1; j <= latime; j++)
+		{
+			lab >> graf[i][j]->penalitate;
+			//salvam indicii din matrice;
+			graf[i][j]->indice_x = i;
+			graf[i][j]->indice_y = j;
+
+
+			k = 1;
+			//adaugam dreapta
+			if (j < latime)
+				graf[i][j]->vecini[k++] = graf[i][j + 1];
+			//adaugam jos
+			if (i < inaltime)
+				graf[i][j]->vecini[k++] = graf[i + 1][j];
+			//adaugam stanga
+			if (i > 1)
+				graf[i][j]->vecini[k++] = graf[i - 1][j];
+			//adaugam sus
+			if (j > 1)
+				graf[i][j]->vecini[k++] = graf[i][j - 1];
+
+			graf[i][j]->numar_vecini = k - 1;
+
+			//valoarea de -1 dicteaza faptul ca nodul este un teleport
+			if (graf[i][j]->penalitate == -1)
+			{
+				//in cazul teleportului, penalitatea este 0
+				graf[i][j]->penalitate = 0;
+				//marcam tile-ul ca fiind teleport
+				graf[i][j]->teleport = true;
+				//salvam legatura cu al doilea teleport, daca acesta este descoperit
+				graf[i][j]->numar_vecini++;
+				//daca nu se cunoaste al doilea teleport, retinem teleportul curent
+				if (gateway == NULL)
+					gateway = graf[i][j];
+				//in schimb, daca il cunoastel, le legam
+				else
+				{
+					graf[i][j]->vecini[graf[i][j]->numar_vecini] = gateway;
+					gateway->vecini[gateway->numar_vecini] = graf[i][j];
+				}
+			}
+		}
+	//distanta dintre primul tile si start este 0 - logic dăăă
+	graf[1][1]->distanta_start = 0;
+	//salvam startul si finish-ul
+	start = graf[1][1];
+	finish = graf[inaltime][latime];
+
+}
+//functia principala de rezolvare
+void rezolvare()
+{
+	//se pleaca de la primul element din matrice
+	for (int i = 1; i <= inaltime; i++)
+		for (int j = 1; j <= latime; j++)
+		{
+			mutari[1][k] = graf[i][j]->indice_x;
+			mutari[2][k] = graf[i][j]->indice_y;
+			//ignoram primul element, deoarece acesta nu are tati
+			if (j != 1 || i != 1)
+			{
+				//apelam fuctia alege_tata(), care ne va returna distanta minima pana la start a tatilor compatibili
+				graf[i][j]->distanta_start = graf[i][j]->alege_tata() + graf[i][j]->penalitate;
+				mutari[3][k] = graf[i][j]->distanta_start;
+				k++;
+				//daca modificam valoarea, actualizam copiii
+				graf[i][j]->actualizare_copii();
+			}
+			else
+			{
+				mutari[3][k] = 0;
+				k++;
+			}
+		}
+	verificare();
+}
+//functie care se asigura ca nu mai avem valori infinite dupa rezolvarea initiala a labirintului
+void verificare()
+{
+	//se pleaca de la primul element din matrice
+	for (int i = 1; i <= inaltime; i++)
+		for (int j = 1; j <= latime; j++)
+		{
+			//daca se descopeta un element cu distanta pana la start infinita, i se alege un tata, si i se modifica aceasta distanta 
+			if (graf[i][j]->distanta_start == INT_MAX)
+			{
+				mutari[1][k] = graf[i][j]->indice_x;
+				mutari[2][k] = graf[i][j]->indice_y;
+				graf[i][j]->distanta_start = graf[i][j]->alege_tata() + graf[i][j]->penalitate;
+				mutari[3][k] = graf[i][j]->distanta_start;
+				k++;
+				//ii actualizam copiii 
+				graf[i][j]->actualizare_copii();
+			}
+		}
+}
+
+
+[STAThreadAttribute]
+
+int main() {
+
+	citire();
+	rezolvare();
+	MyForm mf;
+	int a, numar_elemente_drumuri[12][12] = { 1 };
+	node* aux;
+	for(int i=1;i<=11;i++)
+		for (int j = 1; j <= 11; j++)
+		{
+			if (graf[i][j]->teleport)
+				mf.b[i, j] = -1;
+			else
+				mf.b[i, j] = graf[i][j]->penalitate;
+			aux = graf[i][j];
+			while (aux != start)
+			{
+				mf.drumuri[i, j, numar_elemente_drumuri[i][j], 1] = aux->indice_x;
+				mf.drumuri[i, j, numar_elemente_drumuri[i][j], 2] = aux->indice_y;
+				numar_elemente_drumuri[i][j]++;
+				aux = aux->tata;
+			}
+			mf.drumuri[i, j, numar_elemente_drumuri[i][j], 1] = start->indice_x;
+			mf.drumuri[i, j, numar_elemente_drumuri[i][j], 2] = start->indice_y;
+			mf.drumuri[i, j, 0, 0] = numar_elemente_drumuri[i][j];
+		}
+	for (int i = 1; i < k; i++)
+	{
+		mf.coordonate[1, i] = mutari[1][i];
+		mf.coordonate[2, i] = mutari[2][i];
+		mf.coordonate[3, i] = mutari[3][i];
+		cout << mutari[1][i] << " " << mutari[2][i] << endl;
+	}
+	mf.limita = k - 1;
+	mf.desenare();
+	mf.ShowDialog();
+	return 0;
+}
